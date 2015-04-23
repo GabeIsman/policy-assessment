776af7f29035ccce7bea5e4a0498d1f9ab24295c
Gabe Isman <gabe.isman@gmail.com>
2015-04-22 17:55:18 -0700

    Add source.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421233445) do
+ActiveRecord::Schema.define(version: 20150423005433) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.integer  "response_id"
@@ -19,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150421233445) do
     t.string   "value"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "source"
   end
 
   create_table "assessments", force: :cascade do |t|
@@ -74,8 +78,8 @@ ActiveRecord::Schema.define(version: 20150421233445) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end
