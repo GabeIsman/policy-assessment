00d56cad3bdb41a9ff2eb890deab18f57f778481
Gabe Isman <gabe.isman@gmail.com>
2015-04-25 15:30:40 -0700

    Delegate cocoon event handler

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,7 +1,5 @@
-$(document).ready(function() {
-  $('.question-edit-list').on('cocoon:after-insert', function() {
-    var orderFields = $(this).find('.order-field');
-    var fieldToUpdate = orderFields.last();
-    fieldToUpdate.val(orderFields.length);
-  });
+$(document).on('cocoon:after-insert', '.question-edit-list', function() {
+  var orderFields = $(this).find('.order-field');
+  var fieldToUpdate = orderFields.last();
+  fieldToUpdate.val(orderFields.length);
 });
