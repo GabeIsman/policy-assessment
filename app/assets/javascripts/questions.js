cf571f0fb3cfedcf2d2974572f0dc3a184d6ad46
Gabe Isman <gabe.isman@gmail.com>
2015-05-06 23:52:59 -0700

    Sam's comments

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -3,3 +3,8 @@ $(document).on('cocoon:after-insert', '.question-edit-list', function() {
   var fieldToUpdate = orderFields.last();
   fieldToUpdate.val(orderFields.length);
 });
+
+$(document).ready(function() {
+  // Make all the links in questions open in new tabs
+  $('.question a').each(function() { $(this).attr('target', '_blank'); });
+})
