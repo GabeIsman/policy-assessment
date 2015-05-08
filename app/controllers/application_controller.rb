ab5c97e26bd0b0760a34b6226cdccf4f6547f7e3
Gabe Isman <gabe.isman@gmail.com>
2015-05-07 22:26:19 -0700

    Allow iframe for everything.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :set_tracking_cookie, :set_layout_variables
+  after_action :allow_iframe
 
   protected
 
@@ -27,5 +28,9 @@ class ApplicationController < ActionController::Base
         render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
         return false
       end
+    end
+
+    def allow_iframe
+      response.headers.except! 'X-Frame-Options'
     end
 end
